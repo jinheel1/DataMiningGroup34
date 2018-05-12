dd5c5aab3e6e626b815dfd3346f42c8f4abf0521
jinheel1 <jinheel1@andrew.cmu.edu>
2018-05-12 16:13:32 -0400

    Add files via upload

---
+++ Team34.R
@@ -0,0 +1,20 @@
+
+
+
+
+# Group 34: Maria Rodriguez (mar1); Anirudh Narain (anarain); Kayla Oliva (keoliva); Jinhee Lee (jinheel1)
+
+# Problem 2 (85 points)
+
+## Part 1. Unsupervised Learning
+
+### 1a.
+
+
+
+### 1b.
+
+
+
+## Part 2. Supervised Learning
+
