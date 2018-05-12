9a65e3e56f7045afb85861def79d2dcc46f48123
Maria Rodriguez <mad100141@gmail.com>
2018-05-12 16:47:22 -0400

    initial file

---
+++ Q1ab_Maria_K-Means.R
@@ -0,0 +1,23 @@
+
+
+
+
+# Group 34: Maria Rodriguez (mar1); Anirudh Narain (anarain); Kayla Oliva (keoliva); Jinhee Lee (jinheel1)
+
+# Problem 2 (85 points)
+
+X <- read.table(file="trainX.txt",header=TRUE)
+y <- read.table(file="trainy.txt",header=TRUE)
+
+## Part 1. Unsupervised Learning
+
+### 1a.
+
+
+
+### 1b.
+
+
+
+## Part 2. Supervised Learning
+
